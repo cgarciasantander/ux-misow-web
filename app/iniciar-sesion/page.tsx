8836d5e654f66e352405f3093bd58008cb46e44a
Carlos Garcia <cj.garcias1@uniandes.edu.co>
2025-03-07 09:57:46 -0500

    Adding iniciar sesion

--- app/iniciar-sesion/page.tsx
+++ app/iniciar-sesion/page.tsx
@@ -1,10 +1,40 @@
-import { Container } from "@mui/material";
+import {
+  Box,
+  Button,
+  Container,
+  Divider,
+  TextField,
+  Typography,
+} from "@mui/material";
 
 export default async function IniciarSesion() {
-   
-    return (
-      <Container maxWidth="xl">
-        <h1>Title</h1>
-      </Container>
-    );
-  }
+  return (
+    <Container maxWidth="xs">
+      <Typography variant="h4" component="h1" sx={{mb: "24px"}} >
+        Iniciar sesión
+      </Typography>
+      <Button fullWidth variant="contained">
+        Iniciar sesión con Google
+      </Button>
+      <Divider textAlign="center">ó</Divider>
+      <TextField
+        fullWidth
+        variant="outlined"
+        label="Correo electrónico"
+        margin="normal"
+      />
+      <TextField
+        fullWidth
+        variant="outlined"
+        label="Contraseña"
+        margin="normal"
+      />
+      <Box display="flex" gap="8px">
+        <Button disabled variant="contained">
+          Iniciar sesión
+        </Button>
+        <Button>¿Olvido su contraseña?</Button>
+      </Box>
+    </Container>
+  );
+}
