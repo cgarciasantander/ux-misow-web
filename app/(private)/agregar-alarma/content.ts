d10bf8bf42e6d0652ebf36d48ef2b2d3555e5a47
Fernando Ortiz Suarez <nf.ortiz@uniandes.edu.co>
2025-03-13 21:18:39 +0000

    add add alarma layout

---
+++ app/(private)/agregar-alarma/content.ts
@@ -0,0 +1,6 @@
+export const CONTENT_TEXT = {
+    PAGE: {
+        ADD_ALARM: "Agregar alarma",
+        INSTRUCTIONS: "Introduce el nombre y edad de tu dependiente para identificar facilmente tus recordatorios."
+    }
+}; 
--- app/(private)/agregar-alarma/page.tsx
+++ app/(private)/agregar-alarma/page.tsx
@@ -1,10 +1,53 @@
-import { Container } from "@mui/material";
+import {
+  Container,
+  Stack,
+  Typography,
+} from "@mui/material";
+import { CONTENT_TEXT } from "./content";
 
-export default async function IniciarSesion() {
-   
+const DownloadCard = function ({ title }) {
+  return (
+    <Container sx={{minWidth: "239px", border:  '1px solid #BFC8CC', borderRadius: "12px", minHeight: "300px", backgroundColor: "#F5FAFD", pl: "5px" }}>
+      <Typography variant="subtitle1"  sx={{ mt: "10px", color: "black", fontSize: "14px", fontWeight: 500 }}>
+        {title}
+      </Typography>
+      <Typography sx={{ mt: "10px", color: "black", fontSize: "12px", fontWeight: 400 }}>
+        Escanea el código QR o descarga la aplicación en Apple App Store.
+      </Typography>
+    </Container>
+  )
+}
+
+export default async function AddAlarm() {
+
     return (
       <Container maxWidth="xl">
-        <h1>Title</h1>
+        <Typography variant="h4" component="h1" sx={{ mb: "16px" }}>
+          {CONTENT_TEXT.PAGE.ADD_ALARM}
+        </Typography>
+        <Typography
+          variant="body1"
+          color="textDisabled"
+          component="h1"
+          sx={{ mb: "32px" }}
+        >
+          {CONTENT_TEXT.PAGE.INSTRUCTIONS}
+        </Typography>
+
+        <Typography sx={{ mb: "15px", color: "black", fontSize: "16px", fontWeight: 400 }}>
+          1. Descarga la aplicación movil desde tu celular.
+        </Typography>
+        <Stack direction="row" spacing={2} sx={{ justifyContent: "space-between",  alignItems: "center"}}>
+          <DownloadCard title="Android"/>
+          <DownloadCard title="IOS"/>
+        </Stack>
+        <Typography sx={{ mt: "15px", color: "black", fontSize: "16px", fontWeight: 400, lineHeight: 2 }}>
+          2. Inicia sesión con tu cuenta de ParentCheck. <br />
+          3. Crea una alarma desde la lista de recordatorios. <br />
+          4. Listo, todas tus alarmas se sincronizaran aquí en nuestra aplicación web.
+        </Typography>
       </Container>
     );
   }
+
+
