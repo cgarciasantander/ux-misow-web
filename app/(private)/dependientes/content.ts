2523ef76371b12ac6ab6e8bbc783a5009c97096b
Fernando Ortiz Suarez <nf.ortiz@uniandes.edu.co>
2025-03-13 16:41:26 +0000

    Agregar caso hay dependientes mostrar la lista de dependientes en una tabla

--- app/(private)/dependientes/content.ts
+++ app/(private)/dependientes/content.ts
@@ -1,5 +1,6 @@
 export const CONTENT_TEXT = {
     PAGE: {
+        MY_DEPENDENT: "Mis dependientes",
         START_ADDING_CHILDREN: "Comienza creando una lista de dependientes",
         ADD_CHILD: "Agregar dependiente"
     }
--- app/(private)/dependientes/page.tsx
+++ app/(private)/dependientes/page.tsx
@@ -7,10 +7,39 @@ import { CONTENT_TEXT } from "./content";
 import Link from "next/link";
 import { useUserContext } from "@/hooks/useUser";
 
+
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+
+function createData(
+  name: string,
+  calories: number,
+  fat: number,
+  carbs: number,
+  protein: number,
+) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
+
+
 export default function Dependientes() {
   const { state } = useUserContext();
   console.log(state?.user?.dependents);
-  if (!state?.user?.dependents.length) {
+  if (state?.user?.dependents.length) {
     return (
       <Container maxWidth="xs" sx={{ mt: "130px" }}>
         <Box textAlign="center">
@@ -39,7 +68,57 @@ export default function Dependientes() {
 
   return (
     <Container maxWidth="xl">
-      <h1>Title</h1>
+      <Box sx={{ display: "flex", gap: "8px" }}>
+        <Typography variant="h4" component="h1" sx={{ mb: "16px" }}>
+          {CONTENT_TEXT.PAGE.MY_DEPENDENT}
+        </Typography>
+        <Container>
+          <Button  
+            LinkComponent={Link}
+            href="/agregar-dependiente" 
+            disabled={false} 
+            variant="outlined"             
+            startIcon={<Add />}>
+            Agregar dependiente
+          </Button>
+        </Container>
+      </Box>
+      
+      <BasicTable></BasicTable>
     </Container>
+  );
+}
+
+function BasicTable() {
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Dessert (100g serving)</TableCell>
+            <TableCell align="right">Calories</TableCell>
+            <TableCell align="right">Fat&nbsp;(g)</TableCell>
+            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.calories}</TableCell>
+              <TableCell align="right">{row.fat}</TableCell>
+              <TableCell align="right">{row.carbs}</TableCell>
+              <TableCell align="right">{row.protein}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 }
